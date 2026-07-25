c3248538fe93040aa2a660c462dc73e9153f798b
Nahid Kausar <153551023+justunforgettable@users.noreply.github.com>
2026-07-25 15:06:28 +0530

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,34 @@
+questions = [
+
+{
+"question":"What is the next number in the sequence?\n\n3, 7, 13, 21, 31, ?",
+"options":["39","41","43","45"],
+"answer":"43",
+"category":"Numerical",
+"difficulty":"Medium"
+},
+
+
+{
+"question":"Find the missing number:\n\n4 → 20\n6 → 42\n8 → 72\n10 → ?",
+"options":["90","95","110","120"],
+"answer":"110",
+"category":"Numerical",
+"difficulty":"Hard"
+},
+
+
+{
+"question":"All programmers are problem solvers. Some problem solvers are musicians. Which statement must be true?",
+"options":[
+"Some programmers are musicians",
+"No musicians are programmers",
+"All programmers are problem solvers",
+"All musicians are programmers"
+],
+"answer":"All programmers are problem solvers",
+"category":"Logical",
+"difficulty":"Medium"
+}
+
+]
